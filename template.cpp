de960cbdb101cef642320149fd0415749f02fe7f
MatteoArcari <matteoarcari06@gmail.com>
2023-04-17 22:15:48 +0200

    .

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define INF LLONG_MAX
+#define endl '\n'
+#define int ll
+
+// Non modificare le cose sopra
+
+int solve () {
+    // Leggi l'input del singolo caso 
+    // e restituisci la risposta
+    return 42;
+}
+
+signed main() {
+    // Togli i commenti per leggere da file
+
+    //freopen ("input.txt","r",stdin);
+    //freopen ("output.txt","w",stdout);
+
+    int T; cin >> T;
+    for (int t=1;t<=T;t++) {
+        cout << "Case #" << t << ": " << solve() << endl;
+    }
+
+    return 0;
+}
